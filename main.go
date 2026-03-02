d6bb1eb73307ad040864eca9f6711b7176e6df0d
aripal <dleroari@gmail.com>
2026-03-02 13:24:24 +0100

    adding music

--- main.go
+++ main.go
@@ -1,38 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 var (
-	bg           *ebiten.Image
-	player       *ebiten.Image
-	screenWith   = 480
-	screenHeight = 640
+	bg            *ebiten.Image
+	player        *ebiten.Image
+	screenWith    = 480
+	screenHeight  = 640
+	audioContext  *audio.Context
+	sfxLaunchData []byte
 )
 
 type Game struct {
-	altitude float64
-	speed    float64
-	power    float64
+	altitude     float64
+	speed        float64
+	power        float64
+	launchPlayer *audio.Player
 }
 
 func init() {
 
 	var err error
 
+	// Load background
 	bg, _, err = ebitenutil.NewImageFromFile("assets/background.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Load player sprite
 	player, _, err = ebitenutil.NewImageFromFile("assets/player.png")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Add music
+	sfxLaunchData, err = os.ReadFile("assets/sounds/launch.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func (g *Game) Update() error {
@@ -43,19 +59,24 @@ func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeySpace) {
 		g.power += 0.1
 		g.altitude += g.power
-		log.Printf("g.altitude: %t", g.altitude)
+
+		log.Printf("g.altitude: %d", g.altitude)
 	}
+
+	gravityPull := 0.1
 
 	// Gravity pull down
 	if !ebiten.IsKeyPressed(ebiten.KeySpace) {
 		log.Printf("Space key is not pressed")
+		gravityPull = gravityPull - 10
 		// VelocityY -= gravity * deltatime
-		g.altitude -= 60 * deltatime
+		g.altitude -= 60*deltatime - gravityPull
 	}
 
 	// Rocket is back on track
 	if g.altitude < -5763 {
 		g.altitude = -5763
+		g.power = 0
 	}
 
 	return nil
@@ -80,7 +101,58 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (w, h int) {
 	return screenWith, screenHeight
 }
 
+func playSFX(sfxSound []byte) *audio.Player {
+	// Decode the bytes
+	stream, err := mp3.DecodeWithoutResampling(bytes.NewReader(sfxSound))
+	if err != nil {
+		log.Fatal(stream)
+	}
+
+	// Create player so that we can play, stop etc.
+	musicPlayer, err := audioContext.NewPlayer(stream)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	musicPlayer.Play()
+
+	return musicPlayer
+}
+
+func LoadMusic() {
+	var sampleRate = 48000
+
+	// Load music
+	var err error
+
+	// Create audo context
+	audioContext = audio.NewContext(sampleRate)
+
+	// Read file and return bytes
+	data, err := os.ReadFile("assets/sounds/bossa_nova.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Decode the bytes
+	stream, err := mp3.DecodeWithoutResampling(bytes.NewReader(data))
+	if err != nil {
+		log.Fatal(stream)
+	}
+
+	//
+	musicPlayer, err := audioContext.NewPlayer(stream)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	musicPlayer.Play()
+}
+
 func main() {
+
+	LoadMusic()
+
 	ebiten.SetWindowTitle("Space X")
 	ebiten.SetWindowSize(screenWith, screenHeight)
 
